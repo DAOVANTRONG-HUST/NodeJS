06b324c6ebddf58f56ad493b8bed3eca962e92d4
Đào Văn Trọng <daovantrong.hust@gmail.com>
2019-03-23 17:38:22 +0700

    Bai4

---
+++ nodejs-basic/basic_server.js
@@ -0,0 +1,13 @@
+var http = require("http"); // them mot thu vien
+
+// tạo một server
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hello, This is the first webserver with NodeJS");
+    res.end();
+});
+
+// Lắng nghe biến server
+server.listen(3333, function(){
+    console.log("Server running!!!");
+});
