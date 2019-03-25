1dc42fb4ce2f37165d6e8e2e30af79e743c710e9
Đào Văn Trọng <daovantrong.hust@gmail.com>
2019-03-25 13:02:23 +0700

    default argument

---
+++ Javascript/ES6_default_argument.js
@@ -0,0 +1,13 @@
+// default argument.
+function pay(total = 0, tips = 0) {
+    // tips = tips || 0;
+
+    //     if( tips == undefined)
+    //     {
+    //         tips = 0;
+    //     }
+    //     return total + tips;
+}
+
+
+console.log(pay(1000));
